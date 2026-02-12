05895ec11bea18ce87b30db72fe2031bc82bb0f2
AlonaRahilevych <ragilevic@gmail.com>
2026-02-12 16:29:20 +0100

    feat: implemet functions for crud operations

--- src/entities/workspace/api/WorkspaceService.ts
+++ src/entities/workspace/api/WorkspaceService.ts
@@ -1,8 +1,23 @@
 import api from '@/shared/api/axiosInstance';
-import type { WorkspaceDto, WorkspaceResponse } from '../model/types';
+import type { UpdateWorkspace, Workspace, WorkspaceDto } from '../model/types';
 
 export default class WorkspaceService {
-  static async create(dto: WorkspaceDto): Promise<WorkspaceResponse> {
+  static async create(dto: WorkspaceDto): Promise<Workspace> {
     return await api.post('/workspaces', dto);
+  }
+  static async getAll(): Promise<Workspace[]> {
+    return await api.get('/workspaces');
+  }
+  static async get(id: string): Promise<Workspace> {
+    return await api.get(`/workspaces/${id}`);
+  }
+  static async delete(id: string): Promise<Workspace> {
+    return await api.delete(`/workspaces/${id}`);
+  }
+  static async update(data: {
+    id: string;
+    dto: UpdateWorkspace;
+  }): Promise<Workspace> {
+    return await api.patch(`/workspaces/${data.id}`, data.dto);
   }
 }
