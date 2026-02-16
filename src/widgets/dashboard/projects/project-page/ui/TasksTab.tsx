df6819282951a8d43d2c420a73f1b7edbee410f3
AlonaRahilevych <ragilevic@gmail.com>
2026-02-16 16:23:58 +0100

    commet temporarly imports

--- src/widgets/dashboard/projects/project-page/ui/TasksTab.tsx
+++ src/widgets/dashboard/projects/project-page/ui/TasksTab.tsx
@@ -1,8 +1,8 @@
 import { Inbox } from 'lucide-react';
 
 import { NoDataComponent } from '@/shared/ui/custom/NoDataComponent';
-import { AddButton } from '@/shared/ui/custom/AddButton';
-import { useState } from 'react';
+// import { AddButton } from '@/shared/ui/custom/AddButton';
+// import { useState } from 'react';
 
 // interface ProjectTasksProps {
 //   projectId: string;
@@ -10,14 +10,14 @@ import { useState } from 'react';
 
 export const TasksTab = () => {
   const hasTasks = false;
-  const label = 'Add tasks';
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  //   const label = 'Add tasks';
+  //   const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
     <div className='flex flex-col space-y-4'>
       <div className='flex justify-between items-center'>
         <h3 className='font-semibold'>Project Tasks</h3>
-        <AddButton label={label} onClick={() => setIsModalOpen(true)} />
+        {/* <AddButton label={label} onClick={() => setIsModalOpen(true)} /> */}
       </div>
 
       {!hasTasks ? (
